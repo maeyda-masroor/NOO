9822e52085fee08c42e6fb07c80ee36389168689
maeyda masroor <iosattbsoft2021@gmail.com>
2024-06-15 04:00:08 -0700

    vvv

--- src/component/MultistepForm.js
+++ src/component/MultistepForm.js
@@ -1,61 +1,193 @@
-import React, { useState } from "react";
-import { Steps } from "antd";
-import { Provider } from "./MultiStepFormContext";
-import Details from "./Details";
-import Address from "./Address";
-import Review from "./Reviews";
 
-const { Step } = Steps;
-
-const detailsInitialState = {
-  name: "",
-  age: "",
-  profession: ""
-};
-
-const addressInitialState = {
-  address1: "",
-  address2: "",
-  city: ""
-};
-
-const renderStep = (step) => {
-  switch (step) {
-    case 0:
-      return <Details />;
-    case 1:
-      return <Address />;
-    case 2:
-      return <Review />;
-    default:
-      return null;
-  }
-};
-
-const MultiStepForm = () => {
-  const [details, setDetails] = useState(detailsInitialState);
-  const [address, setAddress] = useState(addressInitialState);
-  const [currentStep, setCurrentStep] = useState(0);
-
-  const next = () => {
-    if (currentStep === 2) {
-      setCurrentStep(0);
-      setDetails(detailsInitialState);
-      setAddress(addressInitialState);
-      return;
+// StepperForm.jsx
+ 
+import React from "react";
+import {
+    makeStyles,
+    Theme,
+    createStyles,
+} from "@material-ui/core/styles";
+import Stepper from "@material-ui/core/Stepper";
+import Step from "@material-ui/core/Step";
+import StepLabel from "@material-ui/core/StepLabel";
+import StepContent from "@material-ui/core/StepContent";
+import Button from "@material-ui/core/Button";
+import Paper from "@material-ui/core/Paper";
+import Typography from "@material-ui/core/Typography";
+ 
+const useStyles = makeStyles((theme) =>
+    createStyles({
+        root: {
+            width: "100%",
+        },
+        button: {
+            marginTop: theme.spacing(1),
+            marginRight: theme.spacing(1),
+        },
+        actionsContainer: {
+            marginBottom: theme.spacing(2),
+        },
+        resetContainer: {
+            padding: theme.spacing(3),
+        },
+    })
+);
+ 
+function getSteps() {
+    return [
+        <b style={{ color: "purple" }}>
+            'Enter Personal Details'
+        </b>,
+        <b style={{ color: "purple" }}>
+            'Enter Education Details'
+        </b>,
+        <b style={{ color: "purple" }}>'Enter Address'</b>,
+    ];
+}
+ 
+function getStepContent(step) {
+    switch (step) {
+        case 0:
+            return (
+                <form class="form-group">
+                    <label>First Name</label>
+                    <input
+                        type="text"
+                        placeholder="First Name"
+                    ></input>
+                    <br></br>
+                    <label>Last Name</label>
+                    <input
+                        type="text"
+                        placeholder="Last Name"
+                    ></input>
+                </form>
+            );
+        case 1:
+            return (
+                <form class="form-group">
+                    <label>High School Percentage</label>
+                    <input
+                        type="number"
+                        placeholder="High School Percentage"
+                    ></input>
+                    <br></br>
+                    <label>Graduation percentage</label>
+                    <input
+                        type="number"
+                        placeholder="Graduation Percentage"
+                    ></input>
+                </form>
+            );
+        case 2:
+            return (
+                <form class="form-group">
+                    <label>Permanent Address</label>
+                    <input
+                        type="text"
+                        placeholder="Permanent Address"
+                    ></input>
+                    <br></br>
+                    <label>Temporary Address</label>
+                    <input
+                        type="text"
+                        placeholder="Temporary Address"
+                    ></input>
+                </form>
+            );
+        default:
+            return "Unknown step";
     }
-    setCurrentStep(currentStep + 1);
-  };
-  const prev = () => setCurrentStep(currentStep - 1);
-  return (
-    <Provider value={{ details, setDetails, next, prev, address, setAddress }}>
-      <Steps current={currentStep}>
-        <Step title={"Fill in your details"} />
-        <Step title={"Address details"} />
-        <Step title={"Review and Save"} />
-      </Steps>
-      <main>{renderStep(currentStep)}</main>
-    </Provider>
-  );
-};
-export default MultiStepForm;
+}
+ 
+export default function GeekStepper() {
+    const classes = useStyles();
+    const [activeStep, setActiveStep] = React.useState(0);
+    const steps = getSteps();
+ 
+    const handleNext = () => {
+        setActiveStep(
+            (prevActiveStep) => prevActiveStep + 1
+        );
+    };
+ 
+    const handleBack = () => {
+        setActiveStep(
+            (prevActiveStep) => prevActiveStep - 1
+        );
+    };
+ 
+    const handleReset = () => {
+        setActiveStep(0);
+    };
+ 
+    return (
+        <div className={classes.root}>
+            <h1>GeeksforGeeks Stepper Form</h1>
+            <Stepper
+                activeStep={activeStep}
+                orientation="vertical"
+            >
+                {steps.map((label, index) => (
+                    <Step key={label}>
+                        <StepLabel>{label}</StepLabel>
+                        <StepContent>
+                            <Typography>
+                                {getStepContent(index)}
+                            </Typography>
+                            <div
+                                className={
+                                    classes.actionsContainer
+                                }
+                            >
+                                <div>
+                                    <Button
+                                        disabled={
+                                            activeStep === 0
+                                        }
+                                        onClick={handleBack}
+                                        className={
+                                            classes.button
+                                        }
+                                    >
+                                        Back
+                                    </Button>
+                                    <Button
+                                        variant="contained"
+                                        color="primary"
+                                        onClick={handleNext}
+                                        className={
+                                            classes.button
+                                        }
+                                    >
+                                        {activeStep ===
+                                        steps.length - 1
+                                            ? "Finish"
+                                            : "Next"}
+                                    </Button>
+                                </div>
+                            </div>
+                        </StepContent>
+                    </Step>
+                ))}
+            </Stepper>
+            {activeStep === steps.length && (
+                <Paper
+                    square
+                    elevation={0}
+                    className={classes.resetContainer}
+                >
+                    <Typography>
+                        Form is submitted
+                    </Typography>
+                    <Button
+                        onClick={handleReset}
+                        className={classes.button}
+                    >
+                        Reset
+                    </Button>
+                </Paper>
+            )}
+        </div>
+    );
+}
